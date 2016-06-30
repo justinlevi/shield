d3a1a8119e0a4966cdb44a531f8a0ee3b0160a20
Christofer VanWormer <christofer.vanwormer1@its.ny.gov>
2016-06-30 16:04:50 -0400

    initial work on eventsubscriber

--- src/EventSubscriber/ShieldEventSubscriber.php
+++ src/EventSubscriber/ShieldEventSubscriber.php
@@ -40,6 +40,31 @@ class ShieldEventSubscriber implements EventSubscriberInterface
    */
     public function kernel_response(Event $event)
     {
-        drupal_set_message('Event kernel.response thrown by Subscriber in module shield.', 'status', true);
+        $config = \Drupal::config('shield.settings');
+        $user = $config->get('shield.user');
+        if (!$user) {
+            \Drupal::logger('shield')->notice('Event kernel.response no user set.');
+            return;
+        }
+
+        /**
+         * @todo allow drush to bypass
+         */
+
+        $pass = $config->get('shield.pass');
+        if (!empty($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])
+            && $_SERVER['PHP_AUTH_USER'] == $user
+            && $_SERVER['PHP_AUTH_PW']   == $pass) {
+            \Drupal::logger('shield')->notice('Event kernel.response variables set.');
+            return;
+        }
+
+        $print = $config->get('shield.print');
+        header(sprintf('WWW-Authenticate: Basic realm="%s"', strtr($print, array('[user]' => $user, '[pass]' => $pass))));
+        header('HTTP/1.0 401 Unauthorized');
+        exit;
+        //drupal_set_message('Event kernel.response thrown by Subscriber in module shield.', 'status', true);
+
+
     }
 }
