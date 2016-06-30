02c04ca731f767652b1fac686693a6f3e5a77659
Justin Winter <justinlevi@gmail.com>
2016-06-30 14:02:43 -0400

    adding KernelEvents

--- src/EventSubscriber/ShieldEventSubscriber.php
+++ src/EventSubscriber/ShieldEventSubscriber.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\shield\EventSubscriber;
 
+use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\EventDispatcher\Event;
 
@@ -10,23 +11,26 @@ use Symfony\Component\EventDispatcher\Event;
  *
  * @package Drupal\shield
  */
-class ShieldEventSubscriber implements EventSubscriberInterface {
+class ShieldEventSubscriber implements EventSubscriberInterface
+{
 
   /**
    * Constructor.
    */
-  public function __construct() {
+    public function __construct()
+    {
 
-  }
+    }
 
   /**
    * {@inheritdoc}
    */
-  static function getSubscribedEvents() {
-    $events['kernel.response'] = ['kernel_response'];
+    static function getSubscribedEvents()
+    {
+        $events['kernel.response'] = ['kernel_response'];
 
-    return $events;
-  }
+        return $events;
+    }
 
   /**
    * This method is called whenever the kernel.response event is
@@ -34,8 +38,8 @@ class ShieldEventSubscriber implements EventSubscriberInterface {
    *
    * @param GetResponseEvent $event
    */
-  public function kernel_response(Event $event) {
-    drupal_set_message('Event kernel.response thrown by Subscriber in module shield.', 'status', TRUE);
-  }
-
+    public function kernel_response(Event $event)
+    {
+        drupal_set_message('Event kernel.response thrown by Subscriber in module shield.', 'status', true);
+    }
 }
